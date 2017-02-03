725639d1d264fd341a415a0deeb8ba5f4003444b
David Mar Gunnarsson <dmrgunnarsson@gmail.com>
2017-02-03 17:40:18 +0000

    AngularFireModule configured in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp({
+      apiKey: " apiKey here ",
+      authDomain: "yourprojectname-xxxxx.firebaseapp.com",
+      databaseURL: "https://yourprojectname-xxxx.firebaseio.com/",
+      storageBucket: ""
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
